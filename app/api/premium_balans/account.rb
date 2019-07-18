904d0744b8eeee549bbd85c2abcb6f593c3149ce
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2019-07-17 22:27:23 -1100

    API: empty prototype.

--- app/api/premium_balans/account.rb
+++ app/api/premium_balans/account.rb
@@ -1,1 +1,20 @@
 # frozen_string_literal: true
+
+# API for exchange with partners
+# analytics, when something from cart
+# is puchaised.
+#Customers can use their premium balans to lower price of the services.
+module Premium_balans
+  class Account < Grape:API
+    # methods
+  end
+  class Cart < Grape:API
+    # methods
+  end
+  class Order < Grape:API
+    # methods
+  end
+  class Service < Grape:API
+    # methods
+  end
+end
