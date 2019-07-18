730e78568a67d633dfdc5f4b16cd8392dd34e0cc
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2019-07-17 22:18:58 -1100

    frozen_string_literal: true changes. And config/application.rb, README.md changes.

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+# frozen_string_literal: true
+
+require File.expand_path('boot', __dir__)
 
 require 'rails/all'
 
@@ -15,7 +17,8 @@ module PremiumBalans
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
